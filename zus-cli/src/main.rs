d30955c5b73aded3491f835bf9b4517d30eccc3f
Jörn Franke <jornfranke@gmail.com>
2021-08-29 21:23:11 +0200

    initial

---
+++ zus-cli/src/main.rs
@@ -0,0 +1,47 @@
+///! ZuSearch Command Line Interface (CLI)
+
+use structopt::StructOpt;
+use zusearch::app;
+use zusearch::app::manager::AppManager;
+
+#[derive(StructOpt,Debug)]
+enum Command {
+    Validate,
+    Trigger,
+    Search
+}
+
+/// Command line arguments
+
+
+#[derive(StructOpt)]
+#[structopt(name = "zus-cli", about = "Command line options for zus-cli")]
+struct Arguments {
+    // The path of the config file to read
+    #[structopt(long="app-file",parse(from_os_str))]
+    appfile: std::path::PathBuf,
+    #[structopt(subcommand)]
+    command: Command
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let args = Arguments::from_args();
+    println!("Welcome to zus-cli!");
+    println!("Checking library zusearch...");
+       // print library version
+    println!("ZuSearch Library version: {}",zusearch::version());
+
+    println!("Loading app definition");
+    println!("{:?}", &args.appfile);
+    println!("Operation {:?}",&args.command);
+
+    let mut appmgr: app::manager::AppList = app::manager::AppManager::new();
+    let f = std::fs::File::open(&args.appfile).expect(&format!("Cannot open file: {}",&args.appfile.as_path().display().to_string()));
+    appmgr.add_app(f).unwrap();
+
+    println!("{}",appmgr.get_app(0).general.name);
+
+ 
+ 
+    Ok(())
+}
